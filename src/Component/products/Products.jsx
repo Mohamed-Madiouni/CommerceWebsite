17cc7abd51b26c1f42479b0467b8bd4ae022a3be
madiouni mohamed <madiounimohamed92@gmail.com>
2021-04-01 16:21:45 +0100

    first commit

---
+++ src/Component/products/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Grid from "@material-ui/core"
+function Products() {
+    return (
+        <main>
+            <Grid container spacing={4} justify="center">
+
+            </Grid>
+        </main>
+    )
+}
+
+export default Products
