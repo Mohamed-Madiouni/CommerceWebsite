87856bb90c058d41c9d26a55a198b1eddf156ed0
madiouni mohamed <madiounimohamed92@gmail.com>
2021-04-01 16:50:00 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Products from './Component/products/Products';
 
 function App() {
   return (
     <div className="App">
-      
+      <Products/>
     </div>
   );
 }
--- src/Component/products/Products.jsx
+++ src/Component/products/Products.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import Grid from "@material-ui/core"
-function Products() {
-    return (
-        <main>
-            <Grid container spacing={4} justify="center">
+import React from "react";
+import { Grid } from "@material-ui/core";
 
-            </Grid>
-        </main>
-    )
+function Products() {
+  return (
+    <main>
+      <Grid container spacing={4} justify="center"></Grid>
+    </main>
+  );
 }
 
-export default Products
+export default Products;
